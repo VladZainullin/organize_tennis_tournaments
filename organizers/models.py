02be43035e1cb4c70cdb5b3c1ddbe474b6d72bbf
Vladislav Zainullin <mittoface02@mail.ru>
2023-04-14 21:34:29 +0300

    Добавляет связь организатора с пользователем

--- organizers/models.py
+++ organizers/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Organizer(models.Model):
@@ -8,6 +9,9 @@ class Organizer(models.Model):
     title = models.CharField(
         max_length=50,
         verbose_name='Наименование')
+    user = models.OneToOneField(
+        to=User,
+        on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'organizers'
