ee6f6d8534781154d1bf3271a36904ebb549400d
Vladislav Zainullin <mittoface02@mail.ru>
2023-05-31 00:02:59 +0300

    Удаляет старую разметку

--- organize_tennis_tournaments/urls.py
+++ organize_tennis_tournaments/urls.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
-from django.urls import path, include
-
-from organize_tennis_tournaments import views
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index')
 ]
