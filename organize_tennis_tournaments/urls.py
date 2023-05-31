e4e6330125b8869a0c3b474a01ecbf0fac18ff73
Vladislav Zainullin <mittoface02@mail.ru>
2023-05-31 20:29:43 +0300

    Добавляет страницу регистрации

--- organize_tennis_tournaments/urls.py
+++ organize_tennis_tournaments/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from organize_tennis_tournaments.views import home_view
+from organize_tennis_tournaments.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home', home_view, name='home'),
+    path('register/', register, name='register')
 ]
--- organize_tennis_tournaments/views.py
+++ organize_tennis_tournaments/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def home_view(request):
     return render(request, 'home.html')
+
+
+def register(request):
+    return render(request, 'authentication/registration/register.html')
