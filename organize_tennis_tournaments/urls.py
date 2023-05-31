c88600c7fbc32893cd3371efbdf3b2713ea98db6
Vladislav Zainullin <mittoface02@mail.ru>
2023-05-31 19:58:50 +0300

    Добавляет базовые страницы

--- organize_tennis_tournaments/urls.py
+++ organize_tennis_tournaments/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from organize_tennis_tournaments.views import home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home', home_view, name='home'),
 ]
